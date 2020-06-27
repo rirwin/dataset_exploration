60a205817c63cd70b45cd94b377c5d78d11c2c31
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2020-06-27 09:35:44 -0700

    some refactoring

--- dataset_creator.py
+++ dataset_creator.py
@@ -24,16 +24,50 @@ from operator import add
 from pyspark.sql import SparkSession
 
 
+def process_dict(d, d_type):
+    if d_type == 'BUSINESS':
+        entry = {
+            'business_id': d['business_id'], 
+            'name': d['name'], 
+            'city': d['city'], 
+            'state': d['state'],
+        }
+    elif d_type == 'REVIEW':
+        entry = {
+            'business_id': d['business_id'],
+            'user_id': d['user_id'],
+            'review_id': d['review_id'],
+            'timestamp': d['date'],
+            'star_rating': d['stars'],
+            'text': d['text'],
+        }
+    elif d_type == 'CHECKIN':
+        entry = d['date'].split(', ')
+    elif d_type == 'TIP':
+        entry = {
+            'business_id': d['business_id'],
+            'user_id': d['user_id'],
+            'text': d['text'],
+            'timestamp': d['date'],
+        }    
+    return [entry]
+
+def create_df_from_file(spark, infile):
+    rdd = spark.read.json(infile).rdd \
+        .map(lambda d: (d['business_id'], process_dict(d, infile_to_type[infile]))) \
+        .reduceByKey(lambda data1, data2: data1 + data2)
+    return spark.createDataFrame(rdd, ['business_id', infile_to_type[infile]])
+
+def join_df_on_business_id(dfs):
+    return reduce(lambda df1, df2: df1.join(df2, ['business_id'], 'left_outer'), dfs)
+
+
 if __name__ == "__main__":
 
     spark = SparkSession\
         .builder\
-        .appName("PythonWordCount")\
+        .appName("YelpBusinessMapper")\
         .getOrCreate()
-
-    #sc = spark.sparkContext
-    #df = spark.read.json(sys.argv[1])
-    #df.printSchema()
 
     infile_to_type = {
         'business_small.json': 'BUSINESS',
@@ -41,52 +75,23 @@ if __name__ == "__main__":
         'review_small.json': 'REVIEW',
         'review_test.json': 'REVIEW',
         'checkin_small.json': 'CHECKIN',
+        'checkin_test.json': 'CHECKIN',
+        'tip_test.json': 'TIP',
     }
-    #lines = spark.read.json(sys.argv[1], multiLine=True)
-    def process_dict(d, d_type):
-        business_id = d['business_id']
-        if d_type == 'BUSINESS':
-            entry = {
-                'business_id': business_id, 
-                'name': d['name'], 
-                'city': d['city'], 
-                'state': d['state']
-            }
-        elif d_type == 'REVIEW':
-            entry = {
-                'business_id': business_id,
-                'review_id': d['review_id'],
-                'timestamp': d['date'],
-                'star_rating': d['stars'],
-                'text': d['text']
-            }
-
-        return (business_id, [entry])
-
-    def create_df_from_file(spark, infile):
-        rdd = spark.read.json(infile).rdd \
-            .map(lambda d: (d['business_id'], process_dict(d, infile_to_type[infile]))) \
-            .reduceByKey(lambda data1, data2: data1 + data2)
-        return spark.createDataFrame(rdd, ['business_id', infile_to_type[infile]])
-
-    def join_df_on_business_id(dfs):
-        return reduce(lambda df1, df2: df1.join(df2, ['business_id'], 'left_outer'), dfs)
 
     business_df = create_df_from_file(spark, 'business_test.json')
     business_reviews_df = create_df_from_file(spark, 'review_test.json')
+    business_checkins_df = create_df_from_file(spark, 'checkin_test.json')
+    business_tips_df = create_df_from_file(spark, 'tip_test.json')
 
-    df = join_df_on_business_id([business_df, business_reviews_df])
+    df = join_df_on_business_id([
+        business_df, 
+        business_reviews_df, 
+        business_checkins_df,
+        business_tips_df,
+    ])
 
+    # consider repartion
     df.write.json('test_write')
-
-    #lines = spark.read.json(list(infile_to_type.keys())).rdd.map(lambda r: (r['business_id'], r))
-    #lines.saveAsTextFile('./test_file')
-    #lines.collect().foreach(println)
-    #counts = lines.flatMap(lambda x: x.split(' ')) \
-    #              .map(lambda x: (x, 1)) \
-    #              .reduceByKey(add)
-    #output = counts.collect()
-    #for (word, count) in output:
-        #print("%s: %i" % (word, count))
 
     spark.stop()
